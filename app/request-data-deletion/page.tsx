a9d90bfec380f65a22768b620fdb4e46ff0ca188
beke benjamin <bekebenjamin@yahoo.com>
2025-07-09 22:12:57 +0100

    update to deletion page

--- app/request-data-deletion/page.tsx
+++ app/request-data-deletion/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     <div className="max-w-xl mx-auto py-12 px-4">
       <h1 className="text-3xl font-bold mb-6">Request Data Deletion</h1>
       <p className="mb-4">
-        If you wish to delete your account and all associated data from FajiBusiness, please send an email to
+        If you wish to delete your account and all associated data from FajiBusiness or FajiBuy, please send an email to
         <span className="font-semibold">{" "}<a href='mailto:contact.fajiapps@gmail.com' style={{textDecoration: 'underline'}}>contact.fajiapps@gmail.com</a></span> with your request. Our team will process your request as soon as possible.
       </p>
       <p className="mb-4">
